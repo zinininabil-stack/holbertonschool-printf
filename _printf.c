145dc16c3f5937a45a2cbc0d6046f2e28aa0a462
zinininabil-stack <zinini.nabil@gmail.com>
2025-11-26 12:39:28 +0100

    Implement _printf function to handle custom format specifiers

---
+++ _printf.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...)
+{
+	va_list list;
+	int i = 0, j, len = 0;
+	checker printers[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}};
+
+	if (!format)
+		return (-1);
+
+	va_start(list, format);
+
+	while (format[i] == '\0')
+	{
+		if (format[i] == '%')
+		{
+			j = 0;
+			while (printers[j].type)
+			{
+				if (format[i + 1] == *(printers[j].type))
+				{
+					len += printers[j].func(list);
+					i += 2;
+					break;
+				}
+				j++;
+			}
+			if (printers[j].type == NULL)
+				len += write(1, &format[i++], 1);
+		}
+		else
+			len += write(1, &format[i++], 1);
+	}
+
+	va_end(list);
+	return (len);
+}
