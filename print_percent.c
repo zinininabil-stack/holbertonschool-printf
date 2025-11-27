3898a1e6685dffb0c9691b199373e557e394d070
zinininabil-stack <zinini.nabil@gmail.com>
2025-11-27 10:15:02 +0100

    Fix indentation in print_percent function for consistency

--- print_percent.c
+++ print_percent.c
@@ -3,6 +3,6 @@
 
 int print_percent(va_list list)
 {
-    (void)list;						/* Print percent character*/
-    return (write(1, "%", 1));
+	(void)list; /* Print percent character*/
+	return (write(1, "%", 1));
 }
