96ad0804bc267c1ce91b8f287bf3589e0975ec62
zinininabil-stack <zinini.nabil@gmail.com>
2025-11-27 11:39:28 +0100

    Add prototype for print_int function in main.h

--- main.h
+++ main.h
@@ -20,5 +20,6 @@ int _printf(const char *format, ...);
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(va_list args);
+int print_int(va_list args);
 
 #endif /* MAIN_H */
