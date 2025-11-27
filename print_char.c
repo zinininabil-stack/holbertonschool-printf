e5ee1fc2d08c726607b3423efdeb69905ca3fa3f
zinininabil-stack <zinini.nabil@gmail.com>
2025-11-27 10:14:46 +0100

    Enhance documentation for print_char function with detailed comments

--- print_char.c
+++ print_char.c
@@ -1,8 +1,17 @@
 #include "main.h"
 #include <unistd.h>
-/* Print regular characters */
+/**
+ * print_char - Imprime un caractère passé via va_list
+ * @list: liste d'arguments contenant l'entier représentant le caractère
+ *
+ * Description: Récupère un int (le caractère) depuis la liste d'arguments
+ *              et l'écrit sur la sortie standard.
+ *
+ * Return: Nombre d'octets écrits (1), ou -1 en cas d'erreur
+ */
 int print_char(va_list list)
 {
-    char c = va_arg(list, int);
-    return (write(1, &c, 1));
+	char c = va_arg(list, int);
+
+	return (write(1, &c, 1));
 }
