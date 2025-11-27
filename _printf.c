2c18e44ebd142fbcd4ae008175e64ddb1f12bfcd
zinininabil-stack <zinini.nabil@gmail.com>
2025-11-27 15:01:12 +0100

    Remove print_int function prototypes from printers array in _printf

--- _printf.c
+++ _printf.c
@@ -11,8 +11,6 @@ int _printf(const char *format, ...)
 		{"c", print_char},
 		{"s", print_string},
 		{"%", print_percent},
-		{"d", print_int}, //
-		{"i", print_int},
 		{NULL, NULL}};
 	if (format == 0)
 		return (-1);
