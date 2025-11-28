d8ea10d4803bc5472c034ef522c5bc2f38b5eb51
zinininabil-stack <zinini.nabil@gmail.com>
2025-11-28 13:51:46 +0100

    Add print_int function prototypes for 'd' and 'i' format specifiers

--- _printf.c
+++ _printf.c
@@ -11,6 +11,8 @@ int _printf(const char *format, ...)
 		{"c", print_char},
 		{"s", print_string},
 		{"%", print_percent},
+		{"d", print_int},  
+    	{"i", print_int}, 
 		{NULL, NULL}};
 	if (format == 0)
 		return (-1);
