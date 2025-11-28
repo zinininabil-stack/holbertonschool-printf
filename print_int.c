409337913a16b289535045b44602d79f16e81f00
zinininabil-stack <zinini.nabil@gmail.com>
2025-11-28 13:50:59 +0100

    Printing integers and decimals

--- print_int.c
+++ print_int.c
@@ -1,9 +1,19 @@
 #include "main.h"
-#include <unistd.h>
-/* Print Integers*/
+
+/**
+ * print_int - prints an integer
+ * @args: va_list containing the integer to print
+ *
+ * Return: number of characters printed
+ */
 
 int print_int(va_list args)
+
 {
-	int n = va_arg(args, int);
-	return (write(1, &n, 1));
+int i = va_arg(args, int);
+char str[12];
+int len = sprintf(str, "%d", i);
+write(1, &str, len);
+
+return (len);
 }
