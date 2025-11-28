1b45a05e7ef75b0ac66ebbefa4d4f4b8c970079b
zinininabil-stack <zinini.nabil@gmail.com>
2025-11-28 13:55:45 +0100

    Add missing header includes in main.h

--- main.h
+++ main.h
@@ -3,6 +3,9 @@
 
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * struct checker - structure for format specifier and corresponding function
